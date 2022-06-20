cf495385ecab3ddbeb7cf97677c218a62425cc1a
nezibana <nezibana0704@outlook.jp>
2022-06-20 19:24:59 +0900

    Update testcode_qyf0237.ino

--- testcode/testcode_qyf0237/testcode_qyf0237.ino
+++ testcode/testcode_qyf0237/testcode_qyf0237.ino
@@ -2,6 +2,7 @@ int xpin = A7;
 int ypin = A6;
 int zpin = A5;
 int acX, acY, acZ;
+double mX, mY, mZ;
 
 void setup() {
   Serial.begin(115200);
@@ -11,12 +12,16 @@ void loop() {
   acX = analogRead(xpin);
   acY = analogRead(ypin);
   acZ = analogRead(zpin);
+  mX = 3*(acX-511.5)/511.5;
+  mY = 3*(acY-511.5)/511.5;
+  mZ = 3*(acZ-511.5)/511.5;
   Serial.print("X:");
-  Serial.println(29.4*(acX-511.5)/511.5);
-  Serial.print("y:");
-  Serial.println(29.4*(acY-511.5)/511.5);
-  Serial.print("z:");
-  Serial.println(29.4*(acZ-511.5)/511.5);
+  Serial.println(9.8*(mX+0.92)/(0.7*(1-0.42)));
+  Serial.print("Y:");
+  Serial.println(9.8*(mY+0.93)/(0.7*(1-0.41)));
+  Serial.print("Z:");
+  Serial.println(9.8*(mZ+0.94)/(0.7*(1-0.42)));
+
   delay(1000);
 }
 /*
