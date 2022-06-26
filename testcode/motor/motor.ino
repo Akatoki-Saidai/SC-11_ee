d76d433dae056bbd16a47b78733e46bbc8ec310b
ddd3h <powerfulnight63d.daisuke@gmail.com>
2022-06-26 23:05:48 +0900

    Update motor.ino
    ブラシレスモーター用のコードに変更

--- testcode/motor/motor.ino
+++ testcode/motor/motor.ino
@@ -1,99 +1,39 @@
-//Aが右，Bが左を想定
-const int A_PHASE = 3;//アナログピンを選択
-const int A_ENABLE = 5;//アナログピンを選択
-const int B_PHASE = 6;//アナログピンを選択
-const int B_ENABLE = 9;//アナログピンを選択
-int i;
+#include <Servo.h>
+
+#define MAX_SIGNAL 2000  //PWM信号における最大のパルス幅[マイクロ秒]
+#define MIN_SIGNAL 1000  //PWM信号における最小のパルス幅[マイクロ秒]
+#define ESC_PIN 6  //ESCへの出力ピン
+int volume;  //可変抵抗の値を入れる変数
+char message[50];  //シリアルモニタへ表示する文字列を入れる変数
+
+Servo esc;  //Servoオブジェクトを作成する．今回はESCにPWM信号を送るので，`esc`と命名している．
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(A_PHASE,OUTPUT);
-  pinMode(A_ENABLE,OUTPUT);
-  pinMode(B_PHASE,OUTPUT);
-  pinMode(B_ENABLE,OUTPUT);
-}
+  Serial.println("Program begin...");
+  Serial.println("This program will calibrate the ESC.");
+  Serial.println("Turn on power source, then wait 2 seconds and press any key.");
+  
+  while (!Serial.available());  //シリアルポートで何か入力されるまで待ちます
+  Serial.read();
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  forward();
-  delay(3000);
-  stopping();
-  delay(3000);
-  back();
-  delay(3000);
-  turn_right();
-  delay(3000);
-  turn_left();
-  delay(3000);
-  accel();
-  delay(1000);
-  decel();
+  esc.attach(ESC_PIN);  //ESCへの出力ピンをアタッチします
+  Serial.println("Writing maximum output.");
+  esc.writeMicroseconds(MAX_SIGNAL);  //ESCへ最大のパルス幅を指示します
+  Serial.println("Wait 2 seconds.");
   delay(2000);
- }
-
-//前進
-int forward(){
- analogWrite(A_ENABLE,255);
- digitalWrite(A_PHASE,HIGH);
- analogWrite(B_ENABLE,255);
- digitalWrite(B_PHASE,HIGH);
+  Serial.println("Writing minimum output");
+  esc.writeMicroseconds(MIN_SIGNAL);  //ESCへ最小のパルス幅を指示します
+  Serial.println("Wait 2 seconds. Then motor starts");
+  delay(2000);
 }
 
-//後退
-int back(){
-   analogWrite(A_ENABLE,255);
-   digitalWrite(A_PHASE,LOW);
-   analogWrite(B_ENABLE,255);
-   digitalWrite(B_PHASE,LOW); 
-}
-
-//静止
-int stopping(){
-   analogWrite(A_ENABLE,0);
-   digitalWrite(A_PHASE,LOW);
-   analogWrite(B_ENABLE,0);
-   digitalWrite(B_PHASE,LOW); 
-}
-
-//右旋回
-int turn_right(){
-   analogWrite(A_ENABLE,0);
-   digitalWrite(A_PHASE,LOW);
-   analogWrite(B_ENABLE,255);
-   digitalWrite(B_PHASE,HIGH); 
-}
-
-//左旋回
-int turn_left(){
-   analogWrite(A_ENABLE,255);
-   digitalWrite(A_PHASE,HIGH);
-   analogWrite(B_ENABLE,0);
-   digitalWrite(B_PHASE,LOW); 
-}
-
-//加速
-int accel(){
-  for(i=5;i==255;i=i+25){
-    analogWrite(A_ENABLE,i);
-    digitalWrite(A_PHASE,HIGH);
-    analogWrite(B_ENABLE,i);
-    digitalWrite(B_PHASE,HIGH);
-    delay(500);
+void loop() {  
+  for(int i = 1000; i <= 2000; i = i + 100){ 
+    volume = i;  //可変抵抗の値を1.0で掛けて変数volumeに入れる．値を調整したい場合は倍率を変更する．
+    sprintf(message, "Pulse Width: %d micro sec", volume);  //シリアルモニタに表示するメッセージを作成
+    Serial.println(message);  //可変抵抗の値をシリアルモニタに表示
+    esc.writeMicroseconds(volume);  // パルス幅 `volume` のPWM信号を送信する
+    delay(1000);
   }
-  forward();
-  delay(3000);
-  stopping();
-}
-
-//減速
-int decel(){
-  for(i=255;i==5;i=i-25){
-    analogWrite(A_ENABLE,i);
-    digitalWrite(A_PHASE,HIGH);
-    analogWrite(B_ENABLE,i);
-    digitalWrite(B_PHASE,HIGH);
-    delay(500); 
-  }
-  stopping();
 }
