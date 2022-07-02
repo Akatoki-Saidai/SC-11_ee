09b95d0ee65f5052d46eda511d5ef8d7d6145e41
Ryusei-Yoshizawa <105530041+Ryusei-Yoshizawa@users.noreply.github.com>
2022-07-02 10:46:50 +0900

    Update testcode_Posture_Estimation.ino

--- testcode/testcode_qyf0237/testcode_Posture_Estimation/testcode_Posture_Estimation.ino
+++ testcode/testcode_qyf0237/testcode_Posture_Estimation/testcode_Posture_Estimation.ino
@@ -1,6 +1,19 @@
 #include<SPI.h>
 #include<MadgwickAHRS.h>
 Madgwick MadgwickFilter;
+//地磁気から
+
+#include <Wire.h>
+#include <DFRobot_QMC5883.h>
+
+DFRobot_QMC5883 compass;
+
+int minX = 0;
+int maxX = 0;
+int minY = 0;
+int maxY = 0;
+int offX = 0;
+int offY = 0;
 
 //gyroよりコピー
 
@@ -74,6 +87,30 @@ void setup() {
                          //   ||++---- BW1-BW0: cut off 12.5[Hz]
                          //   ++------ DR1-DR0: ODR 95[HZ]
   delay(10);
+
+  //地磁気から
+
+   Serial.begin(115200);
+  while (!compass.begin())
+  {
+    Serial.println("Could not find a valid QMC5883 sensor, check wiring!");
+    delay(500);
+  }
+
+    if(compass.isHMC()){
+        Serial.println("Initialize HMC5883");
+        compass.setRange(HMC5883L_RANGE_1_3GA);
+        compass.setMeasurementMode(HMC5883L_CONTINOUS);
+        compass.setDataRate(HMC5883L_DATARATE_15HZ);
+        compass.setSamples(HMC5883L_SAMPLES_8);
+    }
+   else if(compass.isQMC()){
+        Serial.println("Initialize QMC5883");
+        compass.setRange(QMC5883_RANGE_2GA);
+        compass.setMeasurementMode(QMC5883_CONTINOUS); 
+        compass.setDataRate(QMC5883_DATARATE_50HZ);
+        compass.setSamples(QMC5883_SAMPLES_8);
+   }
 }
 
 
@@ -103,6 +140,51 @@ void loop() {
   gy *= 0.00875; // +-250dps
   gz *= 0.00875; // +-250dps
 
+
+//地磁気から
+
+  Vector mag = compass.readRaw();
+
+  if(mag.XAxis>maxX){
+    maxX=mag.XAxis;
+  }
+  if(mag.YAxis>maxY){
+    maxY=mag.YAxis;
+  }
+
+  if(mag.XAxis<minX){
+    minX=mag.XAxis;
+  }
+  if(mag.YAxis<minY){
+    minY=mag.YAxis;
+  }
+  
+  
+
+  Vector norm = compass.readNormalize();
+  
+  // Calculate heading
+  float heading = atan2(norm.YAxis, norm.XAxis);
+
+  // Set declination angle on your location and fix heading
+  // You can find your declination on: http://magnetic-declination.com/
+  // (+) Positive or (-) for negative
+  // For Bytom / Poland declination angle is 4'26E (positive)
+  // Formula: (deg + (min / 60.0)) / (180 / M_PI);
+  float declinationAngle = (-7.0 + (47.0 / 60.0)) / (180 / PI);
+  heading += declinationAngle;
+
+  // Correct for heading < 0deg and heading > 360deg
+  if (heading < 0){
+    heading += 2 * PI;
+  }
+
+  if (heading > 2 * PI){
+    heading -= 2 * PI;
+  }
+
+  // Convert to degrees
+  float headingDegrees = heading * 180/M_PI;
 
   MadgwickFilter.updateIMU(gx,gy,gz,ax,ay,az);
   float roll = MadgwickFilter.getRoll();
