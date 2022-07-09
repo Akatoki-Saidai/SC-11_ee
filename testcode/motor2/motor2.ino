e65e24c5651dcdf4d956019ee73610a14d32b57f
DDD3H <40761694+DDD3H@users.noreply.github.com>
2022-07-09 11:48:02 +0900

    Update motor2.ino

--- testcode/motor2/motor2.ino
+++ testcode/motor2/motor2.ino
@@ -1,6 +1,10 @@
 #include <Servo.h>
 
+<<<<<<< Updated upstream
 #define MAX_SIGNAL 1600  //PWM信号における最大のパルス幅[マイクロ秒]
+=======
+#define MAX_SIGNAL 2000  //PWM信号における最大のパルス幅[マイクロ秒]
+>>>>>>> Stashed changes
 #define MIN_SIGNAL 1000  //PWM信号における最小のパルス幅[マイクロ秒]
 #define ESC_PIN 6  //ESCへの出力ピン
 int volume;  //可変抵抗の値を入れる変数
@@ -21,11 +25,19 @@ void setup() {
   Serial.println("Writing maximum output.");
   esc.writeMicroseconds(MAX_SIGNAL);  //ESCへ最大のパルス幅を指示します
   Serial.println("Wait 2 seconds.");
+<<<<<<< Updated upstream
   delay(4000);
   Serial.println("Writing minimum output");
   esc.writeMicroseconds(MIN_SIGNAL);  //ESCへ最小のパルス幅を指示します
   Serial.println("Wait 2 seconds. Then motor starts");
   delay(4000);
+=======
+  delay(3000);
+  Serial.println("Writing minimum output");
+  esc.writeMicroseconds(MIN_SIGNAL);  //ESCへ最小のパルス幅を指示します
+  Serial.println("Wait 2 seconds. Then motor starts");
+  delay(3000);
+>>>>>>> Stashed changes
   Serial.println("Start 1000 PWM");
     // パルス幅 `volume` のPWM信号を送信する
 }
