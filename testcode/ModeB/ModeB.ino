c3bfdb9a201d9f8d596ffcd97c705f33a55461ce
Ryusei-Yoshizawa <105530041+Ryusei-Yoshizawa@users.noreply.github.com>
2022-07-22 17:48:24 +0900

    Update ModeB.ino

--- testcode/ModeB/ModeB.ino
+++ testcode/ModeB/ModeB.ino
@@ -1,8 +1,11 @@
 #include <Servo.h>
-#include <SPI.h>
 #include <TinyGPS++.h>
 #include <DFRobot_QMC5883.h>
 #include <math.h>
+
+#include <SPI.h>
+#include<MadgwickAHRS.h>
+Madgwick MadgwickFilter;
 
 #define rad2deg(a) ((a) / M_PI * 180.0) /* rad を deg に換算するマクロ関数 */
 #define deg2rad(a) ((a) / 180.0 * M_PI) /* deg を rad に換算するマクロ関数 */
@@ -57,13 +60,8 @@ byte L3GD20_read(byte reg)
   return ret;
 }
 
-
-
-
 DFRobot_QMC5883 compass;
-
-int phase;
-
+int phase = 1;
 double heading;
 double declinationAngle;
 double headingDegrees;
@@ -75,12 +73,9 @@ double Angle_heading;
 double rrAngle, llAngle;
 double sum_latitude = 0;
 double sum_longitude = 0;
-
 double GOAL_lat = 35.860545000;
 double GOAL_lng = 139.606940001;
 double desiredDistance = 10.0; //Mode-Fに移行できる距離
-
-
 // variables___GPS
 //緯度
 double GPSlat_array[5];
@@ -101,22 +96,17 @@ double heading_sum = 0;
 double omega;
 double azimuth;
 double gps_latitude, gps_longitude;
-
 //for Mode-B
 //int CalibrationCounter = 1;
 //int calibration = 1;
 //int sum_count = 0;
 //double CurrentDistance;
 
-
 //モータを動かすプログラム
 int volume;  //可変抵抗の値を入れる変数
 char message[50];  //シリアルモニタへ表示する文字列を入れる変数
-
 Servo escR;
 Servo escL;
-
-
 //右の回転数を上げる
 void RightRotating(){
   escR.writeMicroseconds(1800);
@@ -128,42 +118,30 @@ void LeftRotating(){
   escL.writeMicroseconds(1800);
 }
 
-
 //緯度経度から距離を返す関数
 double CalculateDis(double GOAL_lng, double GOAL_lat, double gps_longitude, double gps_latitude)
 {
-
   GOAL_lng = deg2rad(GOAL_lng);
   GOAL_lat = deg2rad(GOAL_lat);
-
   gps_longitude = deg2rad(gps_longitude);
   gps_latitude = deg2rad(gps_latitude);
-
-  double EarthRadius = 6378.137; //
-
+  double EarthRadius = 6378.137; 
   //目標地点までの距離を導出
   delta_lng = GOAL_lng - gps_longitude;
-
   distance = EarthRadius * acos(sin(gps_latitude) * sin(GOAL_lat) + cos(gps_latitude) * cos(GOAL_lat) * cos(delta_lng)) * 1000;
-
   return distance;
 }
-
 
 //角度計算用の関数
 double CalculateAngle(double GOAL_lng, double GOAL_lat, double gps_longitude, double gps_latitude)
 {
-
   GOAL_lng = deg2rad(GOAL_lng);
   GOAL_lat = deg2rad(GOAL_lat);
-
   gps_longitude = deg2rad(gps_longitude);
   gps_latitude = deg2rad(gps_latitude);
-
   //目標地点までの角度を導出
   delta_lng = GOAL_lng - gps_longitude;
   azimuth = rad2deg(atan2(sin(delta_lng), cos(gps_latitude) * tan(GOAL_lat) - sin(gps_latitude) * cos(delta_lng)));
-
   if (azimuth < 0)
   {
     azimuth += 360;
@@ -178,71 +156,68 @@ double CalculateAngle(double GOAL_lng, double GOAL_lat, double gps_longitude, do
 //機体の向き(heading)の計算
 double CalculateHeading(double heading)
 {
-
   double Sum_headingDegrees=0;
   declinationAngle = (-7.0 + (46.0 / 60.0)) / (180 / PI);
   heading+=declinationAngle;
-
-                if (heading < 0){
-                  heading += 2 * PI;
-                }
-                if (heading > 2 * PI){
-                  heading -= 2 * PI;
-                }
-
-                // Convert to degrees
-                headingDegrees = heading * 180 / M_PI;
-
-                if (headingDegrees < 0){
-                  headingDegrees += 360;
-                }
-
-                if (headingDegrees > 360){
-                  headingDegrees -= 360;
-                }
-
-                for(int i=0;i<15;i++){
-                  Sum_headingDegrees += headingDegrees;
-                }
-
-                return (Sum_headingDegrees/15);
-             
+    if (heading < 0){
+    heading += 2 * PI;
+    }
+    if (heading > 2 * PI){
+    heading -= 2 * PI;
+    }
+    // Convert to degrees
+    headingDegrees = heading * 180 / M_PI;
+    if (headingDegrees < 0){
+    headingDegrees += 360;
+    }
+    if (headingDegrees > 360){
+    headingDegrees -= 360;
+    }
+    for(int i=0;i<15;i++){
+    Sum_headingDegrees += headingDegrees;
+    }
+    return (Sum_headingDegrees/15);             
 }
 
-void setup() {
-  Serial.begin(9600);
+//Posture Estimationから
+int setupco = 0;
+float sumgx, sumgy, sumgz;
+int count = 0;
+long endtime;
+short em_time=0;
+float em_roll=0.0;
+float em_pitch=0.0;
+float em_yaw=0.0;
+bool error_modifying = true;
 
+void setup() {
+  Serial.begin(115200);
+  
+  //RLモーター
   while (!Serial.available());  //シリアルポートで何か入力されるまで待ちます
   Serial.read();
-
   escR.attach(ESC_PIN_R);
   escL.attach(ESC_PIN_L);
-
   Serial.println("Writing maximum output.");
-
   escR.writeMicroseconds(MAX_SIGNAL);
   escL.writeMicroseconds(MAX_SIGNAL);
-
   Serial.println("Wait 2 seconds.");
-  delay(4000);
+  delay(2000);
   Serial.println("Writing minimum output");
-
   escR.writeMicroseconds(MIN_SIGNAL);
   escL.writeMicroseconds(MIN_SIGNAL);
-
   Serial.println("Wait 2 seconds. Then motor starts");
-  delay(4000);
-
+  delay(2000);
+  Serial.println("start!");
+  
+  //posture estimationから
   digitalWrite(SS, HIGH);
   pinMode(SS, OUTPUT);
-  
   SPI.begin();
   SPI.setBitOrder(MSBFIRST);
   SPI.setClockDivider(SPI_CLOCK_DIV8); // 8MHz/8 = 1MHz; (max 10MHz)
-  Serial.println("start!");
- 
+  MadgwickFilter.begin(100);//100H
   Serial.println(L3GD20_read(L3GD20_WHOAMI), HEX);  //should show D4
-  
   L3GD20_write(L3GD20_CTRL1, B00001111);
                          //   |||||||+ X axis enable
                          //   ||||||+- Y axis enable
@@ -250,113 +225,180 @@ void setup() {
                          //   ||||+--- PD: 0: power down, 1: active
                          //   ||++---- BW1-BW0: cut off 12.5[Hz]
                          //   ++------ DR1-DR0: ODR 95[HZ]
+                         
 }
 
-//void loop() {  
-//  for(int i = 100; i <= 2500; i = i + 50){ 
-//    volume = i;  //可変抵抗の値を1.0で掛けて変数volumeに入れる．値を調整したい場合は倍率を変更する．
-//    sprintf(message, "Pulse Width: %d micro sec", volume);  //シリアルモニタに表示するメッセージを作成
-//    Serial.println(message);  //可変抵抗の値をシリアルモニタに表示
-
-//    escR.writeMicroseconds(volume);
-//    escL.writeMicroseconds(volume);
-
-//    delay(500);
-//  }
-//}
 
 
 
 void loop(){
+ //     sprintf(message, "Pulse Width: %d micro sec", 1500);
   switch(phase){
-
-    for(int i = 100; i <= 2500; i = i + 50){ 
-    volume = i;  //可変抵抗の値を1.0で掛けて変数volumeに入れる．値を調整したい場合は倍率を変更する．
-    sprintf(message, "Pulse Width: %d micro sec", volume);  //シリアルモニタに表示するメッセージを作成
-    Serial.println(message);  //可変抵抗の値をシリアルモニタに表示
-
-    escR.writeMicroseconds(volume);
-    escL.writeMicroseconds(volume);
-
-    delay(500);
-        }
-
-
+    
    case 1://Mode-A
    {
-        
-      
       //escR.writeMicroseconds(1500);
       //escL.writeMicroseconds(1500);
 
-      //delay(500);
-      
-      short X, Y, Z;
-      float x, y, z;
-      float kp=2;      //比例ゲインKp
-      float ki=120;    //比例ゲインki
-      float kd=1;      //比例ゲインkd
-      float target=0;         //目標角度[rad]
-    
-      X = L3GD20_read(L3GD20_X_H);
-      x = X = (X << 8) | L3GD20_read(L3GD20_X_L);
-      Y = L3GD20_read(L3GD20_Y_H);
-      y = Y = (Y << 8) | L3GD20_read(L3GD20_Y_L);
-      Z = L3GD20_read(L3GD20_Z_H);
-      z = Z = (Z << 8) | L3GD20_read(L3GD20_Z_L);
-     
-      x *= 0.00875; // +-250dps
-      //x *= 0.0175;// +-500dps
-      //x *= 0.07;  // +-2000dps
-      y *= 0.00875; // +-250dps
-      z *= 0.00875; // +-250dps
-     
-      //Serial.print(X);    // X axis (reading)
-      //Serial.print("\t");
-      //Serial.print(Y);    // Y axis (reading)
-      //Serial.print("\t");
-      //Serial.print(Z);    // Z axis (reading)
-      //Serial.print("\t");
-      Serial.print(x);    // X axis (deg/sec)
-      Serial.print("\t");
-      Serial.print(y);    // Y axis (deg/sec)
-      Serial.print("\t");
-      Serial.println(z);  // Z axis (deg/sec)
-    
-      //PID制御のプログラム
-      static float integral=0; //Iの設定
-      static float last_err=0; //偏差
-      static unsigned long last_micros=0;
-    
-      float current_rad=deg2rad(x); //現在の角度（ロー角）を取得
-            err=current_rad-target;
-    
-            err=abs(err); //errを絶対値に変換
-      
-      float P=kp*err; //Pを計算
-      unsigned long current_micros=micros(); //現在の時間を取得
-      float dt=((float)(current_micros-last_micros))/1000000.0; //経過時間を取得
-    
-      integral+=err*dt; //偏差の積分
-      float I=ki*integral; //Iを計算
-      float diff=(err-last_err)/dt; //偏差の微分を計算
-      float D=kd*diff; 
-      
-      //motor_driver.setMotorSpeed(P+I+D); PIDでモータ制御
-      if(err>10){
-        Serial.println("モータ回転");
-      }else{
-        Serial.println("安定姿勢");
-      }
-      last_err=err; //現在の偏差を保存
-      last_micros=current_micros; //現在の時間を保存
-    
-      delay(100);
-   }
 
+      
+            
+      //姿勢制御追加
+      float Kp = 3;         // 比例ゲインKp
+      float Ki = 3;         // 比例ゲインKi
+      float Kd = 3;         // 比例ゲインKd
+      float target = 0;     // 目標角度[rad]
+      
+        int axpin = A2, aypin = A1, azpin = A0;
+        short mX = analogRead(axpin);
+        short mY = analogRead(aypin);
+        short mZ = analogRead(azpin);
+        //単位を重力加速度(補正済み)
+        float ax = (3*(mX-511.5)/511.5+0.92)/(-0.42);
+        float ay = (3*(mY-511.5)/511.5+0.93)/(-0.41);
+        float az = (3*(mZ-511.5)/511.5+0.94)/(-0.42);
+
+        short X, Y, Z;
+        float gx, gy, gz;//単位を°/s
+        X = L3GD20_read(L3GD20_X_H);
+        gx = X = (X << 8) | L3GD20_read(L3GD20_X_L);
+        Y = L3GD20_read(L3GD20_Y_H);
+        gy = Y = (Y << 8) | L3GD20_read(L3GD20_Y_L);
+        Z = L3GD20_read(L3GD20_Z_H);
+        gz = Z = (Z << 8) | L3GD20_read(L3GD20_Z_L);
+        gx *= 0.00875; // +-250dps
+        //x *= 0.0175;// +-500dps
+        //x *= 0.07;  // +-2000dps
+        gy *= 0.00875; // +-250dps
+        gz *= 0.00875; // +-250dps
+        //角速度誤差補正
+        if(setupco == 0){
+          endtime = millis() + 1000;
+          setupco = 1;
+        }else if(setupco == 1){
+          if(endtime >= millis()){
+            sumgx += gx;
+            sumgy += gy;
+            sumgz += gz;
+            count += 1;
+          }else{
+            sumgx = sumgx/count;
+            sumgy = sumgy/count;
+            sumgz = sumgz/count;
+            setupco = 2;
+          }
+        }else if(setupco == 2){}
+
+        //６軸姿勢推定　MadgwickFilter.updateIMU(角速度x,角速度y,角速度z,加速度x,加速度y,加速度z);
+        MadgwickFilter.updateIMU(gx-sumgx,gy-sumgy,gz-sumgz,ax,ay,az);
+        //ラジアン取得
+        float roll = MadgwickFilter.getRollRadians();
+        float pitch = MadgwickFilter.getPitchRadians();
+        float yaw = MadgwickFilter.getYawRadians();
+       
+        //誤差修正(20秒)
+        if(error_modifying){
+          em_time += 10;
+          if(em_time > 20000){
+          em_roll = roll;
+          em_pitch = pitch;
+          em_yaw = yaw;
+          error_modifying = false;}
+         } 
+
+       //時間短縮用
+        /*Serial.print("e,");//データ識別用
+        Serial.print(roll);
+        Serial.print(",");
+        Serial.print(pitch);
+        Serial.print(",");
+        Serial.println(yaw);*/
+        Serial.print("e,");//データ識別用
+        Serial.print(roll-em_roll);
+        Serial.print(",");
+        Serial.print(pitch-em_pitch);
+        Serial.print(",");
+        Serial.println(yaw-em_yaw);
+
+        //100Ｈｚに合わせる
+      
+      
+      
+      
+      
+        //姿勢制御追加
+        static float integral = 0;
+        static float last_err = 0;
+        static unsigned long last_micros = 0;
+        
+        float current_rad = pitch-em_pitch ;                // センサーから現在の角度を取得
+        float err = target - current_rad;                   // 偏差を計算
+        float P = Kp * err;                                 // Pを計算
+        unsigned long current_micros = micros();            // 現在の時間を取得
+        float dt = ((float)(current_micros - last_micros))
+                    / 1000000.0;                            // 経過時間を計算
+        integral += err * dt;                               // 偏差の積分を計算
+        float I = Ki * integral;                            // Iを計算
+        float diff = (err - last_err) / dt;                 // 偏差の微分を計算
+        float D = Kd * diff;                                // Dを計算
+        
+         escR.writeMicroseconds(1500);
+        escL.writeMicroseconds(1500);       // PIDでモータを制御
+         
+        //右に流されたときの修正
+        //if(roll<-1.0){
+        if(roll-em_roll>1.0/*傾き（ラジアン表記）*/){
+          //現在の時間を取得
+          unsigned long current_micros = micros(); 
+          while(micros()<current_micros + 1000000/*出力時間*/){
+              //機体をyaw方向に+90度回転させる。
+              escR.writeMicroseconds(1500/*入力電圧値*/);
+          }
+          while(micros()<current_micros + 1000000/*出力時間*/){
+              //プロペラを左右回してroll方向の傾きを修正する。    
+              escR.writeMicroseconds(1500/*入力電圧値*/);
+              escL.writeMicroseconds(1500/*入力電圧値*/);
+          }    
+        }
+          //左に流された時の修正
+          //if(roll>-5.0){
+          if(roll-em_roll<-1.0/*傾き（ラジアン表記）*/){
+          //現在の時間を取得
+          unsigned long current_micros = micros(); 
+          while(micros()<current_micros + 1000000/*出力時間*/){
+              //機体をyaw方向に+90度回転させる。
+              escR.writeMicroseconds(1500/*入力電圧値*/);
+          }
+          while(micros()<current_micros + 1000000/*出力時間*/){
+              //プロペラを左右回してroll方向の傾きを修正する。    
+              escR.writeMicroseconds(1500/*入力電圧値*/);
+              escL.writeMicroseconds(1500/*入力電圧値*/);
+          }
+        }
+      
+      
+      
+        //test
+       /* Serial.print(P);
+        Serial.print(",");
+        Serial.print(D);
+        Serial.print(",");
+        Serial.println(I);
+        Serial.print(",");
+        Serial.println(P+D+I);
+        Serial.print(",");*/
+       
+        last_err = err;                                     // 現在の偏差を保存
+        last_micros = current_micros;                       // 現在の時間を保存      
+        delay(10);
+         
+   }
+/*
   case 2://Mode-B
   {
     
+  if(em_time > 20000){
+  
       //escR.writeMicroseconds(1500);
       //escL.writeMicroseconds(1500);
 
@@ -405,8 +447,8 @@ void loop(){
                 LeftRotating();//左側のモータの回転数を上げる
               }
             }
-
-            if(err>0.35)//0.35[rad](約20°以上ロー角傾いたとき)
+          /*
+            if(err>3.0)//0.35[rad](約20°以上ロー角傾いたとき)
             {
               //phase = A;機体が安定していない場合Mode-Aに移行する
               Serial.println("Mode-Aへ移行");
@@ -418,7 +460,7 @@ void loop(){
               //phase = F;距離が理想値よりも小さくなったらMode-Fに移行する
               Serial.println("Mode-Fへ移行");
             }
-        break;
-
-       }
+        break;    
+*/
+}
 }
