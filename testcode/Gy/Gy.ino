d04f3afeee2b460c31ddb373686ea127bf052b14
Ryusei-Yoshizawa <105530041+Ryusei-Yoshizawa@users.noreply.github.com>
2022-08-09 07:45:10 +0900

    Create Gy.ino

---
+++ testcode/Gy/Gy.ino
@@ -0,0 +1,68 @@
+double Gy(double gy, double sumgy){
+    const int L3GD20_CS = SS;
+//const int SS = 10;      // 必ず 10 番を出力にすること
+//const int MOSI = 11;
+//const int MISO = 12;
+//const int SCK  = 13;
+const byte L3GD20_WHOAMI = 0x0f;
+const byte L3GD20_CTRL1 = 0x20;
+const byte L3GD20_CTRL2 = 0x21;
+const byte L3GD20_CTRL3 = 0x22;
+const byte L3GD20_CTRL4 = 0x23;
+const byte L3GD20_CTRL5 = 0x24;
+const byte L3GD20_X_L = 0x28;
+const byte L3GD20_X_H = 0x29;
+const byte L3GD20_Y_L = 0x2A;
+const byte L3GD20_Y_H = 0x2B;
+const byte L3GD20_Z_L = 0x2C;
+const byte L3GD20_Z_H = 0x2D;
+const byte L3GD20_RW = 0x80;
+const byte L3GD20_MS = 0x40;
+
+void L3GD20_write(byte reg, byte val)
+{
+  digitalWrite(L3GD20_CS, LOW);
+  SPI.transfer(reg);
+  SPI.transfer(val);
+  digitalWrite(L3GD20_CS, HIGH);
+}
+byte L3GD20_read(byte reg)
+{
+  byte ret = 0;
+  digitalWrite(L3GD20_CS, LOW);
+  SPI.transfer(reg | L3GD20_RW);
+  ret = SPI.transfer(0);
+  digitalWrite(L3GD20_CS, HIGH);
+  return ret;
+}
+//set upから
+  L3GD20_write(L3GD20_CTRL1, B00001111);
+                         //   |||||||+ X axis enable
+                         //   ||||||+- Y axis enable
+                         //   |||||+-- Z axis enable
+                         //   ||||+--- PD: 0: power down, 1: active
+                         //   ||++---- BW1-BW0: cut off 12.5[Hz]
+                         //   ++------ DR1-DR0: ODR 95[HZ]
+ // loopから
+  short Y;
+  Y = L3GD20_read(L3GD20_Y_H);
+  gy = Y = (Y << 8) | L3GD20_read(L3GD20_Y_L);
+  gy *= 0.00875; // +-250dps
+  //角速度誤差補正
+  if(setupco == 0){
+    endtime = millis() + 1000;
+    setupco = 1;
+  }else if(setupco == 1){
+    if(endtime >= millis()){
+      sumgy += gy;
+      count += 1;
+    }else{
+      sumgy = sumgy/count;
+      setupco = 2;
+    }
+  }else if(setupco == 2){}
+
+  Gy = gy-sumgy;
+
+  return Gy;
+}
