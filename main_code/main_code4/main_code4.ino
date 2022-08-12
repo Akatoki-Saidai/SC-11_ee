ae72c7b8ad53c060440eacc3858a02f8bc7eeb4c
DDD3H <40761694+DDD3H@users.noreply.github.com>
2022-08-12 17:41:33 +0900

    Update main_code4.ino

--- main_code/main_code4/main_code4.ino
+++ main_code/main_code4/main_code4.ino
@@ -145,10 +145,20 @@ void setup(){
     dig_H6 = ((int8_t)dac[6]);
     delay(1000);}
     L3GD20_write(L3GD20_CTRL1, B00001111);
-    compass.setRange(QMC5883_RANGE_2GA);
-    compass.setMeasurementMode(QMC5883_CONTINOUS); 
-    compass.setDataRate(QMC5883_DATARATE_50HZ);
-    compass.setSamples(QMC5883_SAMPLES_8);
+    if(compass.isHMC()){
+        Serial.println("Initialize HMC5883");
+        compass.setRange(HMC5883L_RANGE_1_3GA);
+        compass.setMeasurementMode(HMC5883L_CONTINOUS);
+        compass.setDataRate(HMC5883L_DATARATE_15HZ);
+        compass.setSamples(HMC5883L_SAMPLES_8);
+    }
+   else if(compass.isQMC()){
+        Serial.println("Initialize QMC5883");
+        compass.setRange(QMC5883_RANGE_2GA);
+        compass.setMeasurementMode(QMC5883_CONTINOUS); 
+        compass.setDataRate(QMC5883_DATARATE_50HZ);
+        compass.setSamples(QMC5883_SAMPLES_8);
+   }
 
     escR.attach(ESC_PIN_R);
     escL.attach(ESC_PIN_L);
@@ -177,12 +187,8 @@ void loop(){
     getDataBMP();
     getL3GD20();
     getAcc();
-    headingDegrees = getmagmag();
+    getmagmag();
     sumacc = sqrt(mX*mX + mY*mY + mZ*mZ) -1.0 ;
-//    MadgwickFilter.updateIMU(gyrox,gyroy,gyroz,-mY,-mX,-mZ);
-//    roll = MadgwickFilter.getRollRadians();
-//    pitch = MadgwickFilter.getPitchRadians();
-//    yaw = MadgwickFilter.getYawRadians();
     Serial.println(headingDegrees);
 //    Serial.print("\t");
 //    Serial.println(sumacc);
@@ -358,14 +364,13 @@ float getmagmag(){
   Vector norm = compass.readNormalize();
 
   // Calculate heading
-  float heading = atan2(norm.YAxis, norm.XAxis);
-
+  heading = atan2(norm.YAxis, norm.XAxis);
   // Set declination angle on your location and fix heading
   // You can find your declination on: http://magnetic-declination.com/
   // (+) Positive or (-) for negative
   // For Bytom / Poland declination angle is 4'26E (positive)
   // Formula: (deg + (min / 60.0)) / (180 / M_PI);
-  float declinationAngle = -(9.0 + (2.0 / 60.0)) / (180 / M_PI);
+  float declinationAngle = -(9.0 + (2.0 / 60.0)) / (180 / PI);
   heading += declinationAngle;
 
   // Correct for heading < 0deg and heading > 360deg
@@ -378,8 +383,7 @@ float getmagmag(){
   }
 
   // Convert to degrees
-  float headingDegrees = heading * 180/M_PI;
-  return headingDegrees; 
+  headingDegrees = heading * 180/M_PI;
 }
 
 void LEDsetting(int num){
